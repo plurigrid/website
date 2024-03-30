b9568b7738fe42ec389833dc3c94267c350b02d1
alex <alexander@shyft.network>
2024-03-29 21:01:50 -0700

    hyperlinks, about-us, sizings WIP

--- src/com/plurigrid/home.clj
+++ src/com/plurigrid/home.clj
@@ -11,13 +11,14 @@
   (ui/base
    (assoc ctx ::ui/recaptcha false)
    [:div.flex.justify-between.items-center.bg-orange-50.px-4.py-2
-    [:div.flex.items-center.gap-2
-     [:img.w-20 {:src "/img/plurigrid.png" :alt "Plurigrid logo"}]
-     [:span.text-4xl.font-bold "Plurigrid"]]
+    [:a {:href "https://plurigrid.xyz"}
+     [:div.flex.items-center.gap-2
+      [:img.w-20 {:src "/img/plurigrid.png" :alt "Plurigrid logo"}]
+      [:span.text-4xl.font-bold "Plurigrid"]]]
     [:div.flex.items-center.gap-4
      [:a {:href "/about-us" :class "text-xl font-bold text-gray-800 hover:text-gray-600"} "About Us"]
      [:a {:href "https://vibes.lol" :class "text-xl font-bold text-gray-800 hover:text-gray-600"} "vibes.lol"]
-     [:a {:href "/contact" :class "text-xl font-bold text-gray-800 hover:text-gray-600"} "Contact"]]]
+     [:a {:href "mailto:more@vibes.lol" :class "text-xl font-bold text-gray-800 hover:text-gray-600"} "Contact"]]]
 
    [:.bg-orange-50.flex.flex-col.flex-grow.items-center.p-5
     [:.h-30.grow]
@@ -35,17 +36,22 @@
     [:div.bg-orange-50.flex.flex-col.items-center.p-5
 
      [:div.flex.justify-center.space-x-40.mt-6
+
       [:div.border.dashed.border-2.border-black.w-80.h-64.flex.flex-col.items-center.justify-between.rounded-lg.p-4
        [:div.flex.flex-col.items-center.my-4
         [:h2.text-xl.font-bold.text-center "act"]
         [:p.text-sm.text-center "social cognition platform applied category theory and active inference for situated cognition / microworlding kit"]]
-       [:button {:class "w-3/4 bg-blue-500 text-white font-bold py-2 px-4 rounded mb-3"} "Contribute"]]
+       [:a {:href "https://github.com/plurigrid/act/issues"
+            :class "w-3/4 bg-blue-500 text-white font-bold py-2 px-4 rounded mb-3 text-center block"}
+        "Contribute"]]
 
       [:div.border.dashed.border-2.border-black.w-80.h-64.flex.flex-col.items-center.justify-between.rounded-lg.p-4
        [:div.flex.flex-col.items-center.my-4
         [:h2.text-xl.font-bold.text-center "keep vibin'"]
         [:p.text-sm.text-center "vibes.lol is a compositional world modeling framework for generative models in a consensus topos"]]
-       [:button {:class "w-3/4 bg-blue-500 text-white font-bold py-2 px-4 rounded mb-3"} "Flow"]]]]
+       [:a {:href "http://vibes.lol"
+            :class "w-3/4 bg-blue-500 text-white font-bold py-2 px-4 rounded mb-3 text-center block"}
+        "Flow"]]]]
 
     [:.h-12 {:class "grow-[10]"}]
     [:.text-sm "Member of the post-web ecosystem"]
@@ -56,7 +62,24 @@
    ctx
    [:h2.text-2xl.font-bold "About us"]
    [:p "We're a small team of researchers and developers building tools for "
-    "collaborative research and learning at the mesoscale."]))
+    "collaborative research and learning at the mesoscale."]
+   [:div.grid.grid-cols-2.gap-4
+
+    [:div.flex.flex-col.items-center.w-64  ; Set a fixed width for the container
+     [:img.object-cover.h-64.w-full {:src "/img/bw_alex.jpg" :alt "Description for image 1"}]
+     [:p.text-xs "Alex"]]
+    [:div.flex.flex-col.items-center.w-64
+     [:img.object-cover.h-64.w-full {:src "/img/bw_barton.jpg" :alt "Description for image 2"}]
+     [:p.text-xs "Barton"]]
+    [:div.flex.flex-col.items-center.w-64
+     [:img.object-cover.h-64.w-full {:src "/img/bw_erin.jpg" :alt "Description for image 3"}]
+     [:p.text-xs "Erin "]]
+    [:div.flex.flex-col.items-center.w-64
+     [:img.object-cover.h-64.w-full {:src "/img/bw_nick.jpg" :alt "Description for image 4"}]
+     [:p.text-xs "Nick"]]
+    [:div.flex.flex-col.items-center.w-64
+     [:img.object-cover.h-64.w-full {:src "/img/bw_wiki.jpg" :alt "Description for image 5"}]
+     [:p.text-xs "Wikitoria"]]]))
 
 (def module
   {:routes [["" {:middleware [mid/wrap-redirect-signed-in]}
